a33e46a60287dc76371d11e15bfb8a1184316501
LiliCould <3364724213@qq.com>
2026-05-08 19:32:53 +0800

    feat 配置openapi信息

---
+++ src/main/java/cn/lilicould/liliblog/config/OpenAPIConfig.java
@@ -0,0 +1,65 @@
+package cn.lilicould.liliblog.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI liliBlogOpenAPI() {
+        // 1. 服务器信息：配置你的部署环境
+        Server devServer = new Server()
+                .url("http://localhost:8080") // 开发环境地址
+                .description("开发环境");
+        Server prodServer = new Server()
+                .url("https://lilicould.cn:8888") // 生产环境地址
+                .description("生产环境");
+        // 2. 安全方案 (Security Scheme): 为 JWT 认证做准备
+        // 此处定义了一个名为 "Token验证" 的安全方案，它是一个HTTP Bearer Token，具体为JWT格式。
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("Token验证")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+        // 3. 全局安全要求 (Security Requirement): 默认应用于所有API
+        // 声明所有API都需要满足上述的安全方案。
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Token验证");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("立里博客 LiliBlog API 文档")
+                        .description("这是立里博客的后端接口文档，严格遵循 RESTful 规范设计。欢迎访问线上体验地址。")
+                        .version("v2.0.1")
+                        .termsOfService("https://lilicould.cn/") // 服务条款 URL,暂无
+                        .contact(new Contact()
+                                .name("立里可")
+                                .email("lilicould@qq.com")
+                                .url("https://lilicould.cn/about"))
+                        .license(new License()
+                                .name("MIT 许可证")
+                                .url("https://mit-license.org/"))
+                )
+                // 服务器列表 (Servers)
+                .servers(java.util.Arrays.asList(devServer, prodServer))
+                // 安全组件 (Components)
+                .components(new Components()
+                        .addSecuritySchemes("Token验证", securityScheme))
+                // 安全要求 (Security)
+                .addSecurityItem(securityRequirement)
+                // 外部文档 (External Documentation)
+                .externalDocs(new io.swagger.v3.oas.models.ExternalDocumentation()
+                        .description("查看线上运行版本")
+                        .url("https://lilicould.cn"));
+    }
+}
