a37a5fc7dea688ae62c2fe2f8242da33796a0d8f
LiliCould <3364724213@qq.com>
2026-05-11 19:03:48 +0800

    refactor 重构配置文件结构

--- src/main/java/cn/lilicould/liliblog/config/properties/JwtProperties.java
+++ src/main/java/cn/lilicould/liliblog/config/properties/JwtProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "liliblog.jwt")
+@ConfigurationProperties(prefix = "jwt")
 @Data
 public class JwtProperties {
     private String secret;
