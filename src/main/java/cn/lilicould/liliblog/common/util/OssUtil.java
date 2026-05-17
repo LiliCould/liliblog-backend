9b6fac280375a917384441d92a19fb07e068e038
LiliCould <3364724213@qq.com>
2026-05-17 16:37:46 +0800

    feat 文件上传接口，增加返回值

--- src/main/java/cn/lilicould/liliblog/common/util/OssUtil.java
+++ src/main/java/cn/lilicould/liliblog/common/util/OssUtil.java
@@ -1,19 +1,20 @@
 package cn.lilicould.liliblog.common.util;
 
+import cn.lilicould.liliblog.common.enums.CodeEnum;
+import cn.lilicould.liliblog.common.exception.BusinessException;
 import cn.lilicould.liliblog.config.properties.OssProperties;
 import com.qiniu.common.QiniuException;
 import com.qiniu.http.Response;
 import com.qiniu.storage.Configuration;
 import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
-import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import tools.jackson.databind.ObjectMapper;
 
 import java.io.InputStream;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -33,7 +34,7 @@ public class OssUtil {
 
     // todo 1.使用UUID生成新文件名并上传，同时返回URL
     // todo 2.异常处理
-    public void uploadFile(InputStream inputStream, String fileName) {
+    public String uploadFile(InputStream inputStream, String fileName,String type) throws QiniuException {
 
         Configuration cfg = Configuration.create(Region.createWithRegionId("z0")); // 配置华东地区
         cfg.resumableUploadAPIVersion = Configuration.ResumableUploadAPIVersion.V2;// 指定分片上传版本
@@ -42,22 +43,17 @@ public class OssUtil {
         // 生成上传凭证
         String upToken = getAuthToken();
 
-        try {
-            Response response = uploadManager.put(inputStream,fileName,upToken,null, null);
-            //解析上传成功的结果
-            DefaultPutRet putRet = new ObjectMapper().readValue(response.bodyString(),DefaultPutRet.class);
-            System.out.println(putRet.key);
-            System.out.println(putRet.hash);
-        } catch (QiniuException ex) {
-            if (ex.response != null) {
-                log.error(ex.getMessage());
-                try {
-                    String body = ex.response.toString();
-                    System.err.println(body);
-                } catch (Exception ignored) {
-                    log.error(ignored.getMessage());
-                }
-            }
+        // 生成新的文件名，type为目录，文件名为UUID
+        fileName = type + "/" + UUID.randomUUID().toString() + "_" + fileName;
+
+
+        Response response = uploadManager.put(inputStream,fileName,upToken,null, null);
+        //解析上传成功的结果
+        if (!response.isOK()) {
+            throw new BusinessException(CodeEnum.SYSTEM_ERROR);
         }
+
+        // 返回文件的URL
+        return "https://oss.lilicould.cn" + "/" + fileName;
     }
 }
--- src/main/java/cn/lilicould/liliblog/controller/FileController.java
+++ src/main/java/cn/lilicould/liliblog/controller/FileController.java
@@ -3,6 +3,7 @@ package cn.lilicould.liliblog.controller;
 import cn.lilicould.liliblog.common.result.Result;
 import cn.lilicould.liliblog.common.util.OssUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,10 +28,20 @@ public class FileController {
     @PostMapping("/upload")
     @Operation(summary = "上传文件",description = "文件上传接口")
     @ApiResponse(responseCode = "200",description = "响应成功，登录成功与否看响应状态码")
-    public Result<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    public Result<?> uploadFile(
+            @RequestParam("file") MultipartFile file,
+            @Parameter(description = "上传类型,目前支持：cover,avatar,image,file四个值") @RequestParam(value = "type",required = false) String type
+    ) throws IOException {
+        if (type == null) {
+            type = "file";
+        }
+        // 如果不是cover,avatar,image,file
+        if (!type.equals("cover") && !type.equals("avatar") && !type.equals("image") && !type.equals("file")) {
+            type = "file";
+        }
 
-        ossUtil.uploadFile(file.getInputStream(), file.getOriginalFilename());
+        String url = ossUtil.uploadFile(file.getInputStream(), file.getOriginalFilename(),type);
 
-        return Result.success();
+        return Result.success(url);
     }
 }
